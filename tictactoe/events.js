9574701aea643b6f336f51c432668c8750b5dbe9
Emily Deadman <emilydeadman@Emilys-MBP.home>
2020-07-21 13:50:44 -0400

    updated code

---
+++ tictactoe/events.js
@@ -0,0 +1,32 @@
+const api = require('./api')
+const ui = require('./ui')
+const getFormFields = require('../../../lib/get-form-fields')
+const onSignUp = function (event) {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+  api.signUp(formData)
+  .then(ui.signUpSuccess)
+  .catch(ui.signUpFailure)
+}
+const onSignIn = function (event) {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields()
+  api.signIn(formData)
+  .then(ui.signUpSuccess)
+  .catch(ui.signInFailure)
+}
+const onChangePassword = function (event) {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+  api.changePassword(formData)
+  .then(ui.changePasswordSuccess)
+  .catch(ui.changePasswordFailure)
+}
+const onSignOut = function (event) {
+  event.preventDefault()
+  api.signOut()
+  .then(ui.signOutSuccess)
+}
